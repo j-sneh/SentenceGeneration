ce11b565f478e5e9366bf0ba087ca003fdedfdba
Shlok Mehrotra <shlokmehrotra@gmail.com>
2022-04-25 13:09:24 -0500

    finished rudimentary tests

--- tests/tests.cc
+++ tests/tests.cc
@@ -3,11 +3,39 @@
 
 #include "graph.h"
 #include <iostream>
+#include <string>
 
-TEST_CASE("Non greedy test case check", "Highest Weighted Sentence Check") {
+TEST_CASE("Non greedy test case check", "Trivial Examples") {
   // SECTION("Can use sections") {}
   // clang-format off
-  Graph g ("non_greedy.txt");
-  g.WriteToCSV("result.txt");
+
+  SECTION("Checking Size 1"){
+    string test;
+    Graph g ("tests/non_greedy.txt");
+    test = g.HighestWeightSentence("hello", 1);
+    std::cout << test << std::endl;
+    REQUIRE(test == "hello.");
+    test = g.HighestWeightSentence("my", 1);
+    std::cout << test << std::endl;
+    REQUIRE(test == "my.");
+    test = g.HighestWeightSentence("dear", 1);
+    std::cout << test << std::endl;
+    REQUIRE(test == "dear.");
+  }
+  SECTION("More complex examples"){
+    string test;
+    Graph g ("tests/non_greedy.txt");
+    test = g.HighestWeightSentence("hello", 2);
+    std::cout << test << std::endl;
+    REQUIRE(test == "hello my ");
+    test = g.HighestWeightSentence("lovely", 2);
+    std::cout << test << std::endl;
+    REQUIRE(test == "lovely people ");
+    test = g.HighestWeightSentence("lovely", 3);
+    std::cout << test << std::endl;
+    REQUIRE(test == "lovely ");
+  }
+  
+  
   
 }
