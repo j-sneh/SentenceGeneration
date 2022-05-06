f1f0c9b395bf445fd1b67e36f1d28a3964422539
Jonathan Sneh <jonathaninterweb@gmail.com>
2022-05-06 17:47:42 -0500

    Created Help menu

--- src/main.cc
+++ src/main.cc
@@ -2,25 +2,45 @@
 #include <iostream>
 
 // int main () {
+
+void help() {
+     std::cout << std::endl;
+     std::cout << "OPTIONS: " << std::endl;
+     std::cout << "To pull up this menu:\t\t\t\t\t\"0\"" << std::endl;
+     std::cout << "To generate the highest weight sentence:\t\t\"1 [start word]  [length]\"" << std::endl;
+     std::cout << "To generate a greedy highest-weight sentence:\t\t\"2 [start word]  [length]\"" << std::endl;
+     std::cout << "To generate a greedy lowest-weight sentence:\t\t\"3 [start word]  [length]\"" << std::endl;
+     std::cout << "To generate a random highest-weight sentence:\t\t\"4 [start word]  [length]\"" << std::endl;
+     std::cout << "To generate a random lowest-weight sentence:\t\t\"5 [start word]  [length]\"" << std::endl;
+     std::cout << "To view words that are n-degrees away (BFS):\t\t\"6 [output file] [start word]\"" << std::endl;
+     std::cout << "To view the graph as adjacency lists:\t\t\t\"7 [output file] [start word]\"" << std::endl;
+     std::cout << "To leave the program:\t\t\t\t\t\"8\"" << std::endl;
+     std::cout << std::endl;
+}
 int main (int argc, char** argv) {
-    if (argc == 0) {
-        std::cout << argv[0];
-    }
     /*
     starting bit of parsed_book1.txt
     the boy who lived mr and mrs dursley of number four privet drive were proud
     */
    
-    // if (argc < 3) {
-    //     std::cout << "Usage: " <<  argv[0] << " [filename]" << std::endl;
-    //     return 1;
-    // }
+    if (argc < 2) {
+        std::cout << "Usage: " <<  argv[0] << " [filename]" << std::endl;
+        return 1;
+    }
 
     srand(time(NULL));
     srand(rand());
 
     //Graph g (argv[1]);
-    Graph g ("tests/non_greedy2.txt");
+    Graph g (argv[1]);
+
+    std::cout << argv[1] << " has been parsed into graph!" << std::endl;
+    help();
+
+    while (true) {
+        if ()
+
+    }
     // std::cout <<  g.LowestGreedySentence("hello", 3) << std::endl;
     // std::cout <<  g.LowestGreedySentence("my", 4) << std::endl;
     // std::cout <<  g.LowestGreedySentence("dear", 5) << std::endl;
